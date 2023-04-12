06805cd2eb45281b2d03755c74afb9f7685d9b8a
jsta <stachel2@msu.edu>
2023-04-12 09:12:31 -0600

    formalize rolling figs

--- Makefile
+++ Makefile
@@ -9,7 +9,13 @@ figures/figures.pdf: figures/figures.Rmd
 figures/__rolling_heatmap.pdf: scripts/kelsey.py
 	python $<
 
-figures/all.pdf: figures/figures.pdf
+figures/__rolling_grid.pdf: scripts/kelsey.py
+	python $<
+	echo \\\\pagenumbering{gobble}| cat - mdtable.md > temp && mv temp mdtable.md
+	pandoc mdtable.md -V fontsize=14pt -o $@
+	pdfcrop $@ $@
+
+figures/all.pdf: figures/figures.pdf figures/__rolling_grid.pdf
 	pdftk $< $(wildcard figures/__*.pdf) output $@
 
 clean:
--- scripts/kelsey.py
+++ scripts/kelsey.py
@@ -112,10 +112,11 @@ dt_event = dt_select[[x is not None for x in dt_select["period"]]].copy()
 
 # ---
 
+
 def p_quantile(dep, indep):
     # dep = "fc"
     # indep = "ws"
-    print((dep, indep))    
+    print((dep, indep))
     model = smf.ols("np.log(" + dep + ") ~ " + indep + " * period", data=dt_event).fit()
     p_fl = sm.stats.anova_lm(model, typ=1).to_dict()["PR(>F)"][indep + ":period"]
 
@@ -123,6 +124,7 @@ def p_quantile(dep, indep):
     test = rolling_apply_ext(p_interact, 566, dt[indep].values, dt[dep].values)
     test = test[~np.isnan(test)]
     return stats.percentileofscore(test, p_fl) / 100
+
 
 # p_quantile("co2", "ta") # ~ 0.14
 # p_quantile("fc", "ws")
@@ -132,9 +134,9 @@ if not os.path.exists("data/grid.csv"):
         round(
             abs(
                 p_quantile(
-                    grid.iloc[[i]]["dep"].values[0],
-                    grid.iloc[[i]]["indep"].values[0]
-            )),
+                    grid.iloc[[i]]["dep"].values[0], grid.iloc[[i]]["indep"].values[0]
+                )
+            ),
             2,
         )
         for i in range(grid.shape[0])
@@ -144,10 +146,15 @@ if not os.path.exists("data/grid.csv"):
     grid.to_csv("data/grid.csv", index=False)
 
 grid = pd.read_csv("data/grid.csv")
-test = grid[grid["r2"] > 0.05]
+test = grid[grid["r2"] > 0.05].reset_index(drop=True)
 
-mdtable = tabulate.tabulate(test, tablefmt="github")
-with open('mdtable.md', 'w') as f:
+mdtable = tabulate.tabulate(
+    test,
+    headers=["Explanatory", "Regressor", "R2", r"Event Percentile"],
+    tablefmt="simple",
+    showindex=False,
+)
+with open("mdtable.md", "w") as f:
     f.write(mdtable)
 
 # ---
@@ -157,5 +164,5 @@ with open('mdtable.md', 'w') as f:
 
 sns.lmplot(x="ta", y="co2", hue="period", data=dt_event, scatter_kws={"s": 1})
 # plt.yscale("log")
-plt.show()
+# plt.show()
 min(dt_event["ppfd_in"])
