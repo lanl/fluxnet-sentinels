4addf6223a636001550466d7f18dee4be5f98acf
Jemma Stachelek <jemma.stachelek@gmail.com>
2023-08-21 17:19:05 -0600

    preliminary hyperparam analysis

--- scripts/collect.py
+++ scripts/collect.py
@@ -13,6 +13,7 @@ def parse_f(f_i):
     return {"tolerance": tolerance, "n_days": n_days, "event_quantile": event_quantile}
 
 
+# ---
 flist = glob.glob("data/p_event*")
 flist = list(itertools.compress(flist, [len(f_i) > 30 for f_i in flist]))
 
@@ -20,5 +21,23 @@ dt = [pd.DataFrame(parse_f(flist[i]), index=[0]) for i in range(len(flist))]
 dt = pd.concat(dt)
 dt["p_event"] = [pd.read_csv(flist[i])["pevent"][0] for i in range(len(flist))]
 
-sns.lmplot()
-plt.show()
+dt = pd.melt(
+    dt, id_vars=["p_event"], value_vars=["tolerance", "n_days", "event_quantile"]
+)
+dt["value"] = [float(x) for x in dt["value"]]
+
+sns.lmplot(data=dt, x="value", y="p_event", col="variable", sharex=False)
+plt.savefig("test.pdf")
+
+# ---
+dt = pd.read_csv("data/log.csv")[
+    ["wind_tolerance", "n_days", "event_quantile", "false_positive_rate"]
+]
+dt = pd.melt(
+    dt,
+    id_vars=["false_positive_rate"],
+    value_vars=["wind_tolerance", "n_days", "event_quantile"],
+)
+dt["value"] = [float(x) for x in dt["value"]]
+sns.lmplot(data=dt, x="value", y="false_positive_rate", col="variable", sharex=False)
+plt.savefig("test.pdf")
