9c1e3ba17639f22d2c2cad9e21c711bfaf4365a1
Jemma Stachelek <jemma.stachelek@gmail.com>
2023-10-02 13:16:45 -0600

    also retrieve figures

--- bin/get_data_from_macdesktop.sh
+++ bin/get_data_from_macdesktop.sh
@@ -1,2 +1,3 @@
 # call from top folder level
 scp -r jsta@pn2307690.lanl.gov:/Users/jsta/Documents/Science/LosAlamos/Projects/fluxnet_er/data .
+scp -r jsta@pn2307690.lanl.gov:/Users/jsta/Documents/Science/LosAlamos/Projects/fluxnet_er/figures .
