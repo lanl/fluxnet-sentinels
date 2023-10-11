965a592833691ae1c1d315d918ff7afa2606e4cb
Jemma Stachelek <jemma.stachelek@gmail.com>
2023-10-11 14:05:40 -0600

    formatting

--- figures/fukushima.R
+++ figures/fukushima.R
@@ -12,7 +12,8 @@ coords <- sf::st_as_sf(
 us <- c(left = -126.89, bottom = 41.24, right = -115.83, top = 50.6)
 gg <- get_stamenmap(us, zoom = 5, maptype = "toner-lite") %>%
   ggmap() +
-  geom_point(aes(x = location_long, y = location_lat), data = sites, alpha = .5, color = "red") +
+  geom_point(aes(x = location_long, y = location_lat),
+    data = sites, alpha = .5, color = "red") +
   geom_text(aes(x = location_long, y = location_lat, label = site_id), data = sites)
 
 ggsave("figures/__map_fukushima.pdf", gg)
