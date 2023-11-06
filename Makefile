f3a558c89ccc28b40cc76239b77850efd97cb728
Jemma Stachelek <jsta@lanl.gov>
2023-11-06 15:13:05 -0700

    multi be site data infra

--- Makefile
+++ Makefile
@@ -26,7 +26,8 @@ figures/__footprint.pdf: figures/footprint.py
 figures/__rolling_fukushima_uswrc.pdf: figures/rolling_fukushima.py ../../Data/Asiaflux/FHK.csv
 	python $<
 
-figures/__rolling_fleurus_belon.pdf: figures/rolling_fleurus.py
+figures/__rolling_fleurus_belon.pdf: figures/rolling_fleurus.py \
+	../../Data/Euroflux/BELon.csv ../../Data/Euroflux/BEBra.csv ../../Data/Euroflux/BEVie.csv
 	python $<
 
 figures/all.pdf: figures/__rolling_grid_be-lon.pdf figures/__map.pdf figures/__footprint.pdf \
@@ -39,6 +40,15 @@ data/ameriflux_pnw.csv: scripts/00_get_ameriflux.R
 
 figures/__map_fukushima.pdf: figures/fukushima.R data/ameriflux_pnw.csv
 	Rscript $<
+
+../../Data/Euroflux/BELon.csv: scripts/00_get_euroflux.py
+	python $< --site_id BE-Lon --subfolder L2-L4_2004-2012
+
+../../Data/Euroflux/BEBra.csv: scripts/00_get_euroflux.py
+	python $< --site_id BE-Bra
+
+../../Data/Euroflux/BEVie.csv: scripts/00_get_euroflux.py
+	python $< --site_id BE-Vie
 
 # ---
 clean:
--- scripts/00_get_euroflux.py
+++ scripts/00_get_euroflux.py
@@ -1,21 +1,41 @@
 """
-    Preprocessing Euroflux data files
+    Preprocessing Euroflux data files from:
+    http://www.europe-fluxdata.eu/home/data/request-data
+```
+python scripts/00_get_euroflux.py --site_id BE-Bra
+python scripts/00_get_euroflux.py --site_id BE-Lon --subfolder L2-L4_2004-2012
+```
 """
 import sys
 import glob
+import argparse
 import pandas as pd
 
 sys.path.append(".")
 from src.rolling import preprocess_dt
 
-site_id = "BELon"
+parser = argparse.ArgumentParser()
+parser.add_argument("--site_id", nargs=1, default="", type=str)
+parser.add_argument("--subfolder", nargs=1, default="", type=str)
+userargs = vars(parser.parse_args())
+site = userargs["site_id"][0]
+subfolder = userargs["subfolder"]
+site_code = site.replace("-", "")
+
 dep_cols = ["co2", "fc", "le", "h", "co"]
 indep_cols = ["ws", "p", "pa", "rh", "ppfd_in", "ta", "netrad"]
 
-f_list = glob.glob("../../Data/Euroflux/BE-Lon/L2-L4_2004-2012/*" + site_id + "*.txt")
+if len(subfolder) < 1:
+    f_list_target = "../../Data/Euroflux/" + site + "/*" + site_code + "*.txt"
+else:
+    f_list_target = (
+        "../../Data/Euroflux/" + site + "/" + subfolder[0] + "/*" + site_code + "*.txt"
+    )
+f_list = glob.glob(f_list_target)
 
 dt_list = [
     preprocess_dt(f_list[i], dep_cols, indep_cols)[0] for i in range(len(f_list))
 ]
+
 dt = pd.concat(dt_list).reset_index(drop=True)
-dt.to_csv("../../Data/Euroflux/" + site_id + ".csv", index=False)
+dt.to_csv("../../Data/Euroflux/" + site_code + ".csv", index=False)
