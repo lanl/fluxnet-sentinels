a9b578f6e89342f3ac30c7f3181513c10ba0d381
Jemma Stachelek <jemma.stachelek@gmail.com>
2024-02-29 15:13:28 -0700

    full working grid_all table

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,6 @@ import tabulate
 import subprocess
 import pandas as pd
 
-tabulate.PRESERVE_WHITESPACE = True
-
 
 def amf_clean(dt_raw):
     """
@@ -44,41 +42,48 @@ def pdf_table(
     title,
     path_pdf,
     headers=["Explanatory", "Regressor", "R2", r"Event Percentile"],
-    maxcolwidths=None,
+    # maxcolwidths=None,
+    render_only=False,
 ):
-    if maxcolwidths is None:
-        maxcolwidths = [None for _ in range(dt.shape[1])]
 
-    mdtable = tabulate.tabulate(
-        dt,
-        headers=headers,
-        tablefmt="grid",
-        maxcolwidths=maxcolwidths,
-        showindex=False,
-        numalign="left",
-    )
-    with open("mdtable.md", "w") as f:
-        f.write(mdtable)
+    # if maxcolwidths is None:
+    #     maxcolwidths = [None for _ in range(dt.shape[1])]
 
-    # macos specific commands
-    subprocess.call(
-        'echo "## ' + title + '\n\n$(cat mdtable.md)" > mdtable.md',
-        shell=True,
-    )
-    subprocess.call(
-        'echo "\\pagenumbering{gobble}\n\n$(cat mdtable.md)" > mdtable.md',
-        shell=True,
-    )
+    if not render_only:
+        mdtable = tabulate.tabulate(
+            dt,
+            headers=headers,
+            tablefmt="grid",
+            # maxcolwidths=maxcolwidths,
+            showindex=False,
+            numalign="left",
+        )
+        with open("mdtable.md", "w") as f:
+            f.write(mdtable)
 
-    # linux specific commands
-    # subprocess.call(
-    #     "echo ## " + title + "| cat - mdtable.md > temp && mv temp mdtable.md",
-    #     shell=True,
-    # )
-    # subprocess.call(
-    #     "echo \\pagenumbering{gobble}| cat - mdtable.md > temp && mv temp mdtable.md",
-    #     shell=True,
-    # )
+        # macos specific commands
+        # subprocess.call(
+        #     'echo "## ' + title + '\n\n$(cat mdtable.md)" > mdtable.md',
+        #     shell=True,
+        # )
+        subprocess.call(
+            'echo "\\pagenumbering{gobble}\n\n$(cat mdtable.md)" > mdtable.md',
+            shell=True,
+        )
+        subprocess.call(
+            'echo "---\nheader-includes:\n\t\\usepackage[margin=0.5in]{geometry}\n---\n\n$(cat mdtable.md)" > mdtable.md',
+            shell=True,
+        )
+
+        # linux specific commands
+        # subprocess.call(
+        #     "echo ## " + title + "| cat - mdtable.md > temp && mv temp mdtable.md",
+        #     shell=True,
+        # )
+        # subprocess.call(
+        #     "echo \\pagenumbering{gobble}| cat - mdtable.md > temp && mv temp mdtable.md",
+        #     shell=True,
+        # )
 
     subprocess.call(
         "pandoc --columns=10 mdtable.md -V fontsize=14pt -o " + path_pdf,
--- tables/grid_all.py
+++ tables/grid_all.py
@@ -1,24 +1,34 @@
 import sys
+import itertools
 import pandas as pd
 
 sys.path.append(".")
 from src import utils
 
-flist = ["data/grid_be-lon_7.csv", "data/grid_be-vie_7.csv", "data/grid_be-bra_7.csv"]
-i = 0
-dt = pd.read_csv(flist[i])
-dt = dt[[x != "ppfd_in" for x in dt["indep"]]].reset_index(drop=True)
-dt["site"] = utils.lower_to_mixed_case(flist[i].split("_")[1])
-dt = dt[dt["r2"] > 0].sort_values("r2", ascending=False)
-dt["fquant"] = dt["fquant"] * 100
+
+def process_grid(i):
+    # i = 0
+    dt = pd.read_csv(flist[i])
+    dt = dt[[x != "ppfd_in" for x in dt["indep"]]].reset_index(drop=True)
+    dt["site"] = utils.lower_to_mixed_case(flist[i].split("_")[1])
+    dt = dt[dt["r2"] > 0].sort_values("r2", ascending=False)
+    dt["fquant"] = dt["fquant"] * 100
+    dt = dt.values.tolist()
+    # dt = dt + [SEPARATING_LINE]
+    return dt
+
 
 # NOTE
 # values here are different than the overview table because here they're relative to the POR
 # in the overview table they're limited to only during the event
 
+flist = ["data/grid_be-lon_7.csv", "data/grid_be-vie_7.csv", "data/grid_be-bra_7.csv"]
+grid_list = [process_grid(i) for i in range(len(flist))]
+
 utils.pdf_table(
-    dt,
+    list(itertools.chain(*grid_list)),
     "",
     "tables/grid_all.pdf",
     ["Dep", "Indep", "R2", "Effect", "Site"],
+    render_only=False,
 )
